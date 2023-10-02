2598435f8d39a18af767ea0cb5e9183dd4895940
Ambesawi <ambesawi77@gmail.com>
2023-10-02 13:46:31 -0700

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -0,0 +1,11 @@
+import getListStudents from "./0-get_list_students.js";
+
+console.log(getListStudents());
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 0-main.js 
+[
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+]
