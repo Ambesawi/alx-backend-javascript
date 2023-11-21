759aac1997a5e2b5d54ee56dee7b978d159dd814
Ambesawi <ambesawi77@gmail.com>
2023-11-21 21:18:17 +0000

    2-main_0.js

---
+++ 0x05-Node_JS_basic/2-main_0.js
@@ -0,0 +1,10 @@
+const countStudents = require('./2-read_file');
+
+countStudents("nope.csv");
+
+bob@dylan:~$ node 2-main_0.js
+2-read_file.js:9
+    throw new Error('Cannot load the database');
+    ^
+
+Error: Cannot load the database
